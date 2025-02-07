09cc40e1f91b9650fda4a7b819f4b07dcceed6e0
Liam Sherwood <liam@thesherwoods.de>
2025-02-07 12:52:58 +0100

    test

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,17 +1,17 @@
-const baseURL = "demo.magic-portfolio.com";
+const baseURL = "manrakhan.com";
 
 const routes = {
   "/": true,
   "/about": true,
-  "/work": true,
-  "/blog": true,
-  "/gallery": true,
+  "/work": false,
+  "/blog": false,
+  "/gallery": false,
 };
 
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": false,
 };
 
 const style = {
